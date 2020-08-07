75b961302f7698bd291af4f44b7d5e1d1e813edb
Erick Malta <erickmalta100@gmail.com>
2020-08-06 22:55:33 -0300

    feat: :ok_hand: add options prop to Select

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -5,13 +5,26 @@ import './styles.css';
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   name: string;
   label: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
 }
 
-const Select: React.FC<SelectProps> = ({ label, name, ...rest }) => {
+const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select {...rest} id={name} />
+      <select defaultValue="" {...rest} id={name}>
+        <option value="" disabled hidden>
+          Selecione uma opção
+        </option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
