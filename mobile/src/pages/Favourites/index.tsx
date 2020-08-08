f64f61999d64e324169f853bf6f1d1ac13bf2edc
Erick Malta <erickmalta100@gmail.com>
2020-08-08 17:43:25 -0300

    feat: :ok_hand: add list of favourite teachers

--- mobile/src/pages/Favourites/index.tsx
+++ mobile/src/pages/Favourites/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import styles from './styles';
 
 function Favourites() {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then((res) => {
+      if (res) {
+        const favoritedTeachers = JSON.parse(res);
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  /*  
+  useFocusEffect vem do react-navigation
+  É um método que funciona igual o useEffect, porém, é ativado não toda vez
+  em que a tela é montada, mas sim, quando a tela recebe uma nevagação para ela
+  Já que com navigation bottom tabs o componetne é construído apenas uma vez e depois
+  recebe navegações nele
+  */
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus Proffys favoritos" />
@@ -18,12 +44,9 @@ function Favourites() {
           paddingBottom: 16,
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map((teacher: Teacher) => (
+          <TeacherItem key={teacher.id} teacher={teacher} favorited />
+        ))}
       </ScrollView>
     </View>
   );
